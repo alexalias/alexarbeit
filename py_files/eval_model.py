70e9b60167bacd53661755cf74aea6a4e02dd8cf
Alexandra Krah <12krah@cloak.mafiasi.de>
2017-03-29 11:44:50 +0200

    Slight improvement of the eval_model class.

--- py_files/eval_model.py
+++ py_files/eval_model.py
@@ -15,14 +15,13 @@ actuals_list2 = create_eval_lists.create_modelWLIP_lists(2)[1]
 predictions_list3 = create_eval_lists.create_modelIPdur_lists(2)[0]
 actuals_list3 = create_eval_lists.create_modelIPdur_lists(2)[1]
 
-
+model_name_list = ["WL", "WL_IP", "IP_dur"]
 evaluation_lists = [(predictions_list1, actuals_list1), (predictions_list2, actuals_list2), (predictions_list3, actuals_list3)]
 
-# Evaluate model with: RMSE, MAE, and Pearson's correlation coefficient
-global_rmse, mae = performance_measures.calc_error_for_data(predictions_list2, actuals_list2)
-corrCoef = performance_measures.corrCoef(predictions_list3, actuals_list3)[0]
-print(global_rmse, mae, corrCoef)
-
-#print(predictions_list[:5])
-#print(actuals_list[:5])
-
+# Evaluate models with: RMSE, MAE, and Pearson's correlation coefficient
+for i in range(len(evaluation_lists)):
+	global_rmse, mae = performance_measures.calc_error_for_data(evaluation_lists[i][0], evaluation_lists[i][1])
+	corrCoef = performance_measures.corrCoef(evaluation_lists[i][0], evaluation_lists[i][1])[0]
+	print("RMSE for the" + model_name_list[i] + "-model: " + str(global_rmse))
+	print("MAE for the" + model_name_list[i] + "-model: " + str(mae))
+	print("Pearson corrCoef for the " + model_name_list[i] + "-model: "  + str(corrCoef))
